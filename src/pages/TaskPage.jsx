3bdb8dede7130a8f9808ceaa50974785d9a2e4ea
douglasfnascimento <douglasfnascimento@icloud.com>
2025-06-23 15:11:25 -0300

    Edição na página de detalhes da tarefa

--- src/pages/TaskPage.jsx
+++ src/pages/TaskPage.jsx
@@ -8,7 +8,7 @@ function TaskPage() {
     const title = searchParams.get("title");
     const description = searchParams.get("description");
     return <div className="w-screen h-screen bg-slate-500 p-6">
-        <div className="w-[500px] space-y-4">
+        <div className="w-[500px] mx-auto space-y-4">
         <div className="flex justify-center relative mb-6">
             <button onClick={() => navigate(-1)} className="absolute left-0 text-slate-100">
                 <ChevronLeftIcon />
